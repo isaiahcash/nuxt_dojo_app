4fb7f9faee5730e4885861d5df1467cf4bbbed2c
icash <6e7fde98210b47146de098e6c3cf6070c45fff80>
2023-08-11 11:59:15 -0400

    page setup and routing parameters

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta excepturi tenetur veniam. Nobis, voluptas id voluptatum dolores cupiditate consectetur vero?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos assumenda culpa veritatis animi? Dolore, blanditiis velit! Facilis magni distinctio fuga.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product Details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat amet nam repudiandae accusamus, soluta sequi pariatur eveniet non at. Amet?</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
